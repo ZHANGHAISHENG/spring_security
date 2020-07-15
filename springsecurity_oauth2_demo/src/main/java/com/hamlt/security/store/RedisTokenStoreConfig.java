cd928cb02624eb775182eea2cb6c8fbddefded3f
zhanghaisheng <332627460@qq.com>
2020-07-15 12:11:08 +0800

    更新

--- springsecurity_oauth2_demo/src/main/java/com/hamlt/security/store/RedisTokenStoreConfig.java
+++ springsecurity_oauth2_demo/src/main/java/com/hamlt/security/store/RedisTokenStoreConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
@@ -15,9 +16,13 @@ public class RedisTokenStoreConfig {
     @Autowired
     private RedisConnectionFactory redisConnectionFactory;
 
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
     @Bean
     public TokenStore redisTokenStore(){
-        return new RedisTokenStore(redisConnectionFactory);
+        //return new RedisTokenStore(redisConnectionFactory);
+        return new MyRedisTokenStore(redisConnectionFactory, clientDetailsService);
     }
 
 }
